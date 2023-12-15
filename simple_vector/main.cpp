41ee02c9fee733b66fa9dabe1fa03dc1780aafd0
Shor <danilbug@mail.ru>
2023-12-15 16:50:10 +0300

    Initial Commit

---
+++ simple_vector/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  simple_vector
+//
+//  Created by Даниил Бугаев on 15.12.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
